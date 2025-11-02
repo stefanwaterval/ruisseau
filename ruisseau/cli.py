5d193bd980d9c710e7c7831a06b3bee09da449f8
Stefan Waterval <stefanwaterval@gmail.com>
2025-11-02 11:01:51 +0100

    Add load_dag_from_python function in cli.py

--- ruisseau/cli.py
+++ ruisseau/cli.py
@@ -1,6 +1,10 @@
 import argparse
+import importlib.util
 import sys
+from pathlib import Path
 from typing import Optional, Sequence
+
+from .dag import DAG
 
 
 def build_parser() -> argparse.ArgumentParser:
@@ -33,3 +37,33 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     print("ruisseau validate <path> [--quiet] [--format {auto,yaml,py}]")
     return 0
+
+
+def load_dag_from_python(path: str | Path) -> DAG:
+    if isinstance(path, str):
+        path = Path(path)
+
+    module_name = path.stem
+
+    if not path.exists():
+        raise FileNotFoundError(f"Invalid path: cannot find {path}.")
+
+    if not path.suffix == ".py":
+        raise ValueError(f"Invalid extension: {path} should be a python file.")
+
+    spec = importlib.util.spec_from_file_location(name=module_name, location=path)
+    if spec is None or spec.loader is None:
+        raise ImportError(f"Cannot load module {module_name!r} from {path}")
+
+    module = importlib.util.module_from_spec(spec=spec)
+    spec.loader.exec_module(module)
+
+    if not hasattr(module, "dag"):
+        raise AttributeError(f"{module_name!r} does not define a top-level 'dag'")
+
+    dag = module.dag
+
+    if not isinstance(dag, DAG):
+        raise TypeError(f"{dag} is not of type DAG")
+
+    return dag
