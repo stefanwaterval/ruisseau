bd48f18644fd2cd0a07c86d20a90566438c3b53f
Stefan Waterval <stefanwaterval@gmail.com>
2025-11-16 11:02:09 +0100

    Add __version__

--- ruisseau/__init__.py
+++ ruisseau/__init__.py
@@ -1,7 +1,14 @@
+from importlib.metadata import PackageNotFoundError, version
+
 from ruisseau.dag import DAG
 from ruisseau.executor import LocalExecutor
 from ruisseau.results import DAGResult, TaskResult
 from ruisseau.task import Task
+
+try:
+    __version__ = version("ruisseau")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
 
 __all__ = [
     "DAG",
@@ -9,4 +16,5 @@ __all__ = [
     "LocalExecutor",
     "DAGResult",
     "TaskResult",
+    "__version__",
 ]
